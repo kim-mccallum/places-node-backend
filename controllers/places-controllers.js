73ef86c0a50a74ee7130657f47ee69edb17cf1d8
Kim McCallum <kchaase@gmail.com>
2021-01-16 20:44:40 -0700

    added User model and signup POST

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -129,7 +129,9 @@ const updatePlace = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     console.log(errors);
-    throw new HttpError("Invalid inputs passed, please check your data.", 422);
+    return next(
+      new HttpError("Invalid inputs passed, please check your data.", 422)
+    );
   }
   //extract the id
   const placeId = req.params.pid;
