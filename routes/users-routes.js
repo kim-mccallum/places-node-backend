7f9a1fdd3886a6866225ac7669b141a97cc87026
Kim McCallum <kchaase@gmail.com>
2021-01-28 20:08:59 -0700

    added static image serving

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { check } = require("express-validator");
 
 const usersControllers = require("../controllers/users-controllers");
+const fileUpload = require("../middleware/file-upload");
 
 const router = express.Router();
 
@@ -9,6 +10,7 @@ router.get("/", usersControllers.getUsers);
 //only need to validate the sign up route as validation is implicit on login
 router.post(
   "/signup",
+  fileUpload.single("image"), //use multer middleware to take a single file in the image key
   [
     check("name").not().isEmpty(),
     check("email").normalizeEmail().isEmail(),
