d4d78987e2a150853fec6149139b28789e224e23
Kim McCallum <kchaase@gmail.com>
2021-03-14 21:23:34 -0600

    added new react app frontend

--- app.js
+++ app.js
@@ -18,26 +18,32 @@ const app = express();
 app.use(bodyParser.json());
 //all files are locked down by default but we can use middleware to serve images
 app.use("/uploads/images", express.static(path.join("uploads", "images"))); //add middleware to serve static images
+app.use(express.static(path.join("public")));
 
-// add middleware to handle CORS
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*"); //* allows any domain to send request
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Orgin, X-Requested-With, Content-Type, Accept, Authorization"
-  ); //this controls which headers the incoming request can have
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-  next();
-});
+// // add middleware to handle CORS
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*"); //* allows any domain to send request
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Orgin, X-Requested-With, Content-Type, Accept, Authorization"
+//   ); //this controls which headers the incoming request can have
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+//   next();
+// });
 
 app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
 
-//middleware to catch requests to unsupported routes
+// any request that reaches backend not handled by routes above, goes here. Serve React staticly
 app.use((req, res, next) => {
-  const error = new HttpError("Could not find this route.", 404);
-  throw error;
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
 });
+
+// //middleware to catch requests to unsupported routes
+// app.use((req, res, next) => {
+//   const error = new HttpError("Could not find this route.", 404);
+//   throw error;
+// });
 
 //error handling in express - adding error means that this function only triggers with an error
 app.use((error, req, res, next) => {
---
+++ public/precache-manifest.81c583af47073002b77ed92b0115bbd9.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c0ec679a8f0d9448942a49e23d1d625",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90a98ab227ae2302f2aa",
+    "url": "/static/css/0.ab9953c4.chunk.css"
+  },
+  {
+    "revision": "39c044625c7d24b0c335",
+    "url": "/static/css/4.124dad99.chunk.css"
+  },
+  {
+    "revision": "5aaa920aa62dca666fbd",
+    "url": "/static/css/5.b1909d64.chunk.css"
+  },
+  {
+    "revision": "015a0e7296a3cb74e088",
+    "url": "/static/css/6.992ddf32.chunk.css"
+  },
+  {
+    "revision": "9498fc34d3932ff6f5b6",
+    "url": "/static/css/7.0f75d51a.chunk.css"
+  },
+  {
+    "revision": "766a4e31c93eb3400ea4",
+    "url": "/static/css/8.50b710da.chunk.css"
+  },
+  {
+    "revision": "d82e3de331f49b5f9bf0",
+    "url": "/static/css/main.81467fd3.chunk.css"
+  },
+  {
+    "revision": "90a98ab227ae2302f2aa",
+    "url": "/static/js/0.d85fa16f.chunk.js"
+  },
+  {
+    "revision": "b42e36e2af8fe4b69e87",
+    "url": "/static/js/3.d67c4e61.chunk.js"
+  },
+  {
+    "revision": "39c044625c7d24b0c335",
+    "url": "/static/js/4.248d0093.chunk.js"
+  },
+  {
+    "revision": "5aaa920aa62dca666fbd",
+    "url": "/static/js/5.c53efc69.chunk.js"
+  },
+  {
+    "revision": "015a0e7296a3cb74e088",
+    "url": "/static/js/6.d2a73373.chunk.js"
+  },
+  {
+    "revision": "9498fc34d3932ff6f5b6",
+    "url": "/static/js/7.8d13e13d.chunk.js"
+  },
+  {
+    "revision": "766a4e31c93eb3400ea4",
+    "url": "/static/js/8.187205a7.chunk.js"
+  },
+  {
+    "revision": "d82e3de331f49b5f9bf0",
+    "url": "/static/js/main.9208c81a.chunk.js"
+  },
+  {
+    "revision": "601192524e20e3bc2dee",
+    "url": "/static/js/runtime-main.1569ec65.js"
+  }
+]);
